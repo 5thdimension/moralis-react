47ece22c981e4e5b89fbe8895efe26472e1dc35e
5thdimension <jesse.charron@live.ca>
2021-10-20 01:56:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import * as fcl from "@onflow/fcl";
 function App() {
   const [user, setUser] = useState({loggedIn: null})
   const [name, setName] = useState('')
+  const [transactionStatus, setTransactionStatus] = useState(null) // NEW
 
   useEffect(() => fcl.currentUser.subscribe(setUser), [])
 
@@ -23,7 +24,6 @@ function App() {
     setName(profile?.name ?? 'No Profile')
   }
 
-  // NEW
   const initAccount = async () => {
     const transactionId = await fcl.mutate({
       cadence: `
@@ -52,13 +52,39 @@ function App() {
     console.log(transaction)
   }
 
+  // NEW
+  const executeTransaction = async () => {
+    const transactionId = await fcl.mutate({
+      cadence: `
+        import Profile from 0xProfile
+
+        transaction(name: String) {
+          prepare(account: AuthAccount) {
+            account
+              .borrow<&Profile.Base{Profile.Owner}>(from: Profile.privatePath)!
+              .setName(name)
+          }
+        }
+      `,
+      args: (arg, t) => [arg("Flow Developer!", t.String)],
+      payer: fcl.authz,
+      proposer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 50
+    })
+
+    fcl.tx(transactionId).subscribe(res => setTransactionStatus(res.status))
+  }
+
   const AuthedState = () => {
     return (
       <div>
         <div>Address: {user?.addr ?? "No Address"}</div>
         <div>Profile Name: {name ?? "--"}</div>
+        <div>Transaction Status: {transactionStatus ?? "--"}</div> {/* NEW */}
         <button onClick={sendQuery}>Send Query</button>
-        <button onClick={initAccount}>Init Account</button> {/* NEW */}
+        <button onClick={initAccount}>Init Account</button>
+        <button onClick={executeTransaction}>Execute Transaction</button> {/* NEW */}
         <button onClick={fcl.unauthenticate}>Log Out</button>
       </div>
     )
